553a43363a9807339749b08cd55399de27b483e4
Linux CLI <zakim019.211@spwtstudent.net>
2021-02-28 12:52:13 +0000

    🚚💵 CPU IRQ Support

--- os/cpu/isr.c
+++ os/cpu/isr.c
@@ -8,7 +8,10 @@
 #include "isr.h"
 #include "idt.h"
 #include "../drivers/screen.h"
+#include "../drivers/ports.h"
 #include "../kernel/util.h"
+
+isr_t interruptHandlers[256];
 
 void isr_install()
 {
@@ -44,6 +47,36 @@ void isr_install()
     set_idt_gate(29, (u32)isr29);
     set_idt_gate(30, (u32)isr30);
     set_idt_gate(31, (u32)isr31);
+
+    // PIC
+    port_byte_out(0x20, 0x11);
+    port_byte_out(0xA0, 0x11);
+    port_byte_out(0x21, 0x20);
+    port_byte_out(0xA1, 0x28);
+    port_byte_out(0x21, 0x04);
+    port_byte_out(0xA1, 0x02);
+    port_byte_out(0x21, 0x01);
+    port_byte_out(0xA1, 0x01);
+    port_byte_out(0x21, 0x0);
+    port_byte_out(0xA1, 0x0);
+
+    // IRQ
+    set_idt_gate(32, (u32)irq0);
+    set_idt_gate(33, (u32)irq1);
+    set_idt_gate(34, (u32)irq2);
+    set_idt_gate(35, (u32)irq3);
+    set_idt_gate(36, (u32)irq4);
+    set_idt_gate(37, (u32)irq5);
+    set_idt_gate(38, (u32)irq6);
+    set_idt_gate(39, (u32)irq7);
+    set_idt_gate(40, (u32)irq8);
+    set_idt_gate(41, (u32)irq9);
+    set_idt_gate(42, (u32)irq10);
+    set_idt_gate(43, (u32)irq11);
+    set_idt_gate(44, (u32)irq12);
+    set_idt_gate(45, (u32)irq13);
+    set_idt_gate(46, (u32)irq14);
+    set_idt_gate(47, (u32)irq15);
 
     set_idt(); // Load with assembly
 }
@@ -84,9 +117,10 @@ char *exception_messages[] = {
     "Reserved",
     "Reserved",
     "Reserved",
-    "Reserved"
-};
-void isr_handler(registers_t r) {
+    "Reserved"};
+
+void isr_handler(registers_t r)
+{
     kprint("received an interrupt: ");
     char s[3];
     asciiIntConverter(r.int_no, s);
@@ -94,4 +128,23 @@ void isr_handler(registers_t r) {
     kprint("\n");
     kprint(exception_messages[r.int_no]);
     kprint("\n");
+}
+
+void register_interrupt_handler(u8 n, isr_t handler)
+{
+
+    interruptHandlers[n] = handler;
+}
+
+void irq_handler(registers_t r)
+{
+    if (r.int_no >= 40)
+        port_byte_out(0xA0, 0x20); // slave
+    port_byte_out(0x20, 0x20);     // master
+
+    if (interruptHandlers[r.int_no] != 0)
+    {
+        isr_t handler = interruptHandlers[r.int_no];
+        handler(r);
+    }
 }
